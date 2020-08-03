06fb15ba6b0b7f475561546c9023820d90f8fd36
TERRY WONG <iamtktwong@gmail.com>
2020-08-03 11:00:34 +0800

    add keycloak support

--- src/main/java/hk/org/ha/pms/test/demo/UserController.java
+++ src/main/java/hk/org/ha/pms/test/demo/UserController.java
@@ -3,6 +3,7 @@ package hk.org.ha.pms.test.demo;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
@@ -19,21 +20,25 @@ public class UserController {
         return "hello all!!";
     }
 
+    @RolesAllowed("admin")
     @PostMapping("/users")
     User createUser(@Valid @RequestBody User user) {
         return userService.create(user);
     }
 
+    @RolesAllowed("user")
     @GetMapping("/users")
     List<User> getAll() {
        return userService.findAll();
     }
 
+    @RolesAllowed("admin")
     @PutMapping("/users")
     User updateUser(@Valid @RequestBody User userDetail) {
         return userService.update(userDetail);
     }
 
+    @RolesAllowed("admin")
     @DeleteMapping("/users/{id}")
     Map<String, Boolean> deleteUser(@PathVariable Long id) {
         userService.delete(id);
